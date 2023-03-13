1e7b740c9489c058f9ab42e9d514c1a7f6414011
QoB <kackar212@gmail.com>
2023-03-13 22:31:55 +0100

    Add css module for CartButton

--- src/components/cart-button/cart-button.component.tsx
+++ src/components/cart-button/cart-button.component.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
+import styles from "./cart-button.module.scss";
 
 export function CartButton() {
   return (
-    <button>
+    <button className={styles.button}>
       <Image
         src="assets/shopping-cart.svg"
         alt="Open items list"
