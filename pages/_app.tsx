06add4981b1d02ff9691651bdc94169d118a9e8e
QoB <kackar212@gmail.com>
2023-03-13 20:43:26 +0100

    Create global.scss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import "../styles/normalize.css";
+import "../styles/global.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
