c6d9bd42b27f634a2a226d5ea6e972bec977ff8a
QoB <kackar212@gmail.com>
2023-03-19 05:52:41 +0100

    Add style for current link

--- src/components/nav-list-item/nav-list-item.component.tsx
+++ src/components/nav-list-item/nav-list-item.component.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import styles from "./nav-list-item.module.scss";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 interface NavListItemProps {
   href: string;
@@ -11,10 +12,19 @@ interface NavListItemProps {
 }
 
 export function NavListItem({ href, icon }: NavListItemProps) {
+  const router = useRouter();
+  const ariaCurrent = router.route === href ? "page" : undefined;
+
   return (
     <li className={styles.listItem}>
-      <Link href={href} className={styles.link}>
-        <Image src={icon.src} alt={icon.alt} width={24} height={24} />
+      <Link href={href} className={styles.link} aria-current={ariaCurrent}>
+        <Image
+          src={icon.src}
+          alt={icon.alt}
+          width={24}
+          height={24}
+          className={styles.icon}
+        />
       </Link>
     </li>
   );
