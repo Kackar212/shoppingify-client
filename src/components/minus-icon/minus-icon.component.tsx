f724cb068533cf6e2aa179124cc7d6b7c3fea0cc
QoB <kackar212@gmail.com>
2023-03-26 10:54:58 +0200

    Create MinusIcon component

---
+++ src/components/minus-icon/minus-icon.component.tsx
@@ -0,0 +1,19 @@
+export function MinusIcon({ color = "#F9A109", className = "" }) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#000000"
+      strokeWidth="0.4800000000000001"
+      fill={color}
+      className={className}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M6 12C6 11.4477 6.44772 11 7 11H17C17.5523 11 18 11.4477 18 12C18 12.5523 17.5523 13 17 13H7C6.44772 13 6 12.5523 6 12Z"
+        fill="#000000"
+      ></path>
+    </svg>
+  );
+}
