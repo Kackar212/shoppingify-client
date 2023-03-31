8d141b46d7ad3a87be39624761ea768a2ad1fad7
QoB <kackar212@gmail.com>
2023-03-31 15:46:16 +0200

    Create Error component that will display input error

---
+++ src/components/error/error.component.tsx
@@ -0,0 +1,30 @@
+import { ErrorMessage } from "@hookform/error-message";
+import { useFormContext } from "react-hook-form";
+import { VisuallyHidden } from "../visually-hidden/visually-hidden.component";
+import styles from "./error.module.scss";
+
+interface ErrorProps {
+  errorId: string;
+  name: string;
+}
+
+export function Error({ errorId, name }: ErrorProps) {
+  const {
+    formState: { errors },
+  } = useFormContext();
+
+  return (
+    <ErrorMessage
+      errors={errors}
+      name={name}
+      render={({ message }) => {
+        return (
+          <span id={errorId} className={styles.error} aria-live="polite">
+            <VisuallyHidden>Error: </VisuallyHidden>
+            {message}
+          </span>
+        );
+      }}
+    />
+  );
+}
