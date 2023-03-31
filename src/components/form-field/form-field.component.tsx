0b91f2c1ccbe3ffac0868c9d0e143594d9411fde
QoB <kackar212@gmail.com>
2023-03-31 17:00:18 +0200

    Create FormField component

---
+++ src/components/form-field/form-field.component.tsx
@@ -0,0 +1,39 @@
+import { FieldErrors, FieldValues, useFormContext } from "react-hook-form";
+import { ReactNode } from "react";
+import styles from "./form-field.module.scss";
+import { Label } from "../label/label.component";
+import { Error } from "../error/error.component";
+
+interface FormFieldProps {
+  name: string;
+  label: string;
+  render: (
+    props: Omit<FormFieldProps, "render"> & {
+      errors: FieldErrors<FieldValues>;
+    }
+  ) => ReactNode;
+  required?: boolean;
+  placeholder?: string;
+  errorId?: string;
+}
+
+export function FormField({
+  name,
+  label,
+  render,
+  required = false,
+  placeholder = `Enter a ${label.toLowerCase()}`,
+  errorId = `${name}-error`,
+}: FormFieldProps) {
+  const {
+    formState: { errors },
+  } = useFormContext();
+
+  return (
+    <div className={styles.field}>
+      <Label text={label} name={name} required={required} />
+      <Error errorId={errorId} name={name} />
+      {render({ name, label, errors, required, errorId, placeholder })}
+    </div>
+  );
+}
