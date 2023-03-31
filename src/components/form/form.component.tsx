34b4f912874786b8a3315eeec1b5821d9606d209
QoB <kackar212@gmail.com>
2023-04-01 00:55:38 +0200

    Create Form component

---
+++ src/components/form/form.component.tsx
@@ -0,0 +1,32 @@
+import { FormHTMLAttributes, ReactNode } from "react";
+import {
+  useForm,
+  FormProvider,
+  FieldValues,
+  SubmitHandler,
+  UseFormProps,
+} from "react-hook-form";
+
+interface FormProps<T extends FieldValues>
+  extends Omit<FormHTMLAttributes<HTMLFormElement>, "onSubmit"> {
+  onSubmit: SubmitHandler<T>;
+  children: ReactNode;
+  options: UseFormProps<T>;
+}
+
+export function Form<T extends FieldValues>({
+  onSubmit,
+  children,
+  options,
+  ...attrs
+}: FormProps<T>) {
+  const methods = useForm<T>(options);
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)} {...attrs}>
+        {children}
+      </form>
+    </FormProvider>
+  );
+}
