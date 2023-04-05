86b861c421bd659114c32dd54a3d358d22b960eb
QoB <kackar212@gmail.com>
2023-04-05 15:09:24 +0200

    Add logic for adding product to list in AddProductButton component

--- src/components/add-product-button/add-product-button.component.tsx
+++ src/components/add-product-button/add-product-button.component.tsx
@@ -1,6 +1,13 @@
 import { VisuallyHidden } from "../visually-hidden/visually-hidden.component";
 import { PlusIcon } from "../plus-icon/plus-icon.component";
 import styles from "./add-product-button.module.scss";
+import {
+  useAddProductToListMutation,
+  useGetActiveListQuery,
+} from "../../features/api";
+import { useCallback } from "react";
+import { Button } from "../button/button.component";
+import { useMutationDebounce } from "../../hooks/useDebounceMutation";
 
 interface AddProductButtonProps {
   id: number;
@@ -8,10 +15,42 @@ interface AddProductButtonProps {
 }
 
 export function AddProductButton({ name, id }: AddProductButtonProps) {
+  const [addProductToList, { isLoading }] = useMutationDebounce(
+    useAddProductToListMutation
+  );
+  const { data } = useGetActiveListQuery();
+
+  const addProduct = useCallback(() => {
+    if (!data) {
+      return;
+    }
+
+    const {
+      data: { products },
+    } = data;
+
+    const isProductAlreadyInList = !!products.find(
+      ({ product, isDeleted }) => product.id === id && !isDeleted
+    );
+
+    if (isProductAlreadyInList) {
+      // TODO Show user an error
+      return console.error("Product is already in your list!");
+    }
+
+    addProductToList(id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id, data]);
+
   return (
-    <button type="button" className={styles.button}>
+    <Button
+      type="button"
+      className={styles.button}
+      onClick={addProduct}
+      isLoading={isLoading}
+    >
       <PlusIcon color="#000" />
       <VisuallyHidden>Add {name} to list</VisuallyHidden>
-    </button>
+    </Button>
   );
 }
