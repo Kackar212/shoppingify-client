359b7359420db40fec84895bb692910e7a144d4f
KacKar212 <kackar212@gmail.com>
2023-04-08 12:24:50 +0200

    Setup react-toastify

--- src/components/layout/layout.component.tsx
+++ src/components/layout/layout.component.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from "react";
 import { Navigation } from "../navigation/navigation.component";
 import styles from "./layout.module.scss";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 interface LayoutProps {
   children: ReactNode;
@@ -12,6 +14,12 @@ export function Layout({ children, className }: LayoutProps) {
     <div className={`${className} ${styles.container}`}>
       <Navigation />
       <main className={styles.main}>{children}</main>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        newestOnTop={true}
+        theme="dark"
+      />
     </div>
   );
 }
