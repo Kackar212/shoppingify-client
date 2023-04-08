648559dac790f30a143aadb09dff7371324e1bff
KacKar212 <kackar212@gmail.com>
2023-04-08 17:10:21 +0200

    Create LoginForm component

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,6 +1,9 @@
 export const API_ERROR = {
+  WRONG_CREDENTIALS: "Provided email or password are incorrect!",
   TIMEOUT: "Request has taken too long!",
   GENERIC: "Something went wrong! Try again later!",
 } as const;
+
+export const API_SUCCESS = { LOGGED_IN: "You have logged in!" };
 
 export const VALIDATION_MODE_ALL = "all";
---
+++ src/components/login-form/login-form.component.tsx
@@ -0,0 +1,51 @@
+import { useCallback } from "react";
+import { LoginBody } from "../../common/interfaces/login-body.interface";
+import { useGetErrorMessage } from "../../hooks/useGetErrorMessage";
+import { useLoginMutation } from "../../features/api";
+import { API_SUCCESS } from "../../common/constants";
+import { toast } from "react-toastify";
+import * as yup from "yup";
+import { FormFieldInput } from "../../common/interfaces/form-field-input.interface";
+import { AuthFormContent } from "../auth-form-content/auth-form-content.component";
+import { AuthForm } from "../auth-form/auth-form.component";
+
+export const inputs: FormFieldInput[] = [
+  { name: "email", label: "Email" },
+  { name: "password", label: "Password", type: "password" },
+];
+
+export const signInSchema = yup
+  .object({
+    email: yup.string().email().required(),
+    password: yup.string().min(8).max(30).required(),
+  })
+  .required();
+
+export function LoginForm() {
+  const [signInUser, { error, isSuccess, isLoading }] = useLoginMutation();
+  const errorMessage = useGetErrorMessage(error);
+
+  const signIn = useCallback(
+    async (data: LoginBody) => {
+      if (isLoading) {
+        return;
+      }
+
+      toast.promise(signInUser(data), { pending: "Wait" });
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [isLoading]
+  );
+
+  return (
+    <AuthForm<LoginBody> schema={signInSchema} onSubmit={signIn}>
+      <AuthFormContent
+        inputs={inputs}
+        isSuccess={isSuccess}
+        heading="Sign in"
+        errorMessage={errorMessage}
+        successMessage={API_SUCCESS.LOGGED_IN}
+      />
+    </AuthForm>
+  );
+}
