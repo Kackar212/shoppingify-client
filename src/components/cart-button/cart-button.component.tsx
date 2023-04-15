b70e9386fcab380c6304222196eb892b721700e1
QoB <kackar212@gmail.com>
2023-04-15 08:37:34 +0200

    Add logic responsible for toggling shopping list

--- src/components/cart-button/cart-button.component.tsx
+++ src/components/cart-button/cart-button.component.tsx
@@ -1,21 +1,35 @@
 import Image from "next/image";
 import styles from "./cart-button.module.scss";
 import { VisuallyHidden } from "../visually-hidden/visually-hidden.component";
+import { useDispatch } from "react-redux";
+import { toggle } from "../../features/slices/shopping-list.slice";
+import { useCallback } from "react";
 
 interface CartButtonProps {
   items?: number;
 }
 
 export function CartButton({ items }: CartButtonProps) {
+  const dispatch = useDispatch();
+
+  const toggleShoppingList = useCallback(() => {
+    dispatch(toggle());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <button type="button" className={styles.button}>
+    <button
+      type="button"
+      className={styles.button}
+      onClick={toggleShoppingList}
+    >
       <Image
         src="/assets/shopping-cart.svg"
         alt="Open items list"
         width={20}
         height={20}
       />
-      {items && (
+      {!!items && (
         <div className={styles.items}>
           <VisuallyHidden> - items: </VisuallyHidden>
           {items}
