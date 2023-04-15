e9a7f29653ff165b771fd55b6a4c3ea9cf93a681
QoB <kackar212@gmail.com>
2023-04-15 10:49:25 +0200

    Add logic to ShoppingList component

--- src/components/shopping-list/shopping-list.component.tsx
+++ src/components/shopping-list/shopping-list.component.tsx
@@ -1,5 +1,36 @@
 import { ReactElement } from "react";
+import { useGetActiveListQuery } from "../../features/api";
+import { ShoppingListProducts } from "../shopping-list-products/shopping-list-products.component";
+import { CreateProduct } from "../create-product/create-product.component";
+import { SaveListForm } from "../save-list-form/save-list-form.component";
+import styles from "./shopping-list.module.scss";
+import { ChangeStatus } from "../change-status/change-status.component";
+import { selectShoppingList } from "../../features/slices/shopping-list.slice";
+import { useSelector } from "react-redux";
 
 export function ShoppingList(): ReactElement {
-  return <></>;
+  const { data: shoppingList, name } = useGetActiveListQuery(undefined, {
+    selectFromResult(state) {
+      return {
+        ...state,
+        name: state.data?.data.name,
+        data: state.data?.data,
+      };
+    },
+  });
+
+  const { isOpen } = useSelector(selectShoppingList);
+
+  return (
+    <section className={isOpen ? styles.shoppingListOpen : styles.shoppingList}>
+      <CreateProduct />
+      <h2>{name}</h2>
+      <ShoppingListProducts />
+      {shoppingList && name ? (
+        <ChangeStatus shoppingListId={shoppingList?.id} />
+      ) : (
+        <SaveListForm />
+      )}
+    </section>
+  );
 }
