2108ca838681bec188913171084e90f379a19de6
QoB <kackar212@gmail.com>
2023-04-17 00:50:11 +0200

    Refactor sign up page to contain relevant links

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,5 +1,15 @@
 import { RegisterForm } from "../../src/components/register-form/register-form.component";
+import { FormFooter } from "../../src/components/form-footer/form-footer.component";
+import { Link } from "../../src/components/link/link.component";
 
 export default function Login() {
-  return <RegisterForm />;
+  return (
+    <section>
+      <RegisterForm />
+      <FormFooter>
+        You already have account?
+        <Link href="/auth/login">Sign in</Link>
+      </FormFooter>
+    </section>
+  );
 }
