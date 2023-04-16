8b78d4ba533f2ac5d3c270b1bc12c496789f5dae
QoB <kackar212@gmail.com>
2023-04-17 00:49:11 +0200

    Refactor sign in page to contain relevant links

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,5 +1,21 @@
 import { LoginForm } from "../../src/components/login-form/login-form.component";
+import { FormFooter } from "../../src/components/form-footer/form-footer.component";
+import { Link } from "../../src/components/link/link.component";
 
 export default function Login() {
-  return <LoginForm />;
+  return (
+    <section>
+      <LoginForm />
+      <FormFooter>
+        <p>
+          You don&apos;t have account?{" "}
+          <Link href="/auth/register">Sign up</Link>
+        </p>
+        <p>
+          You forgot password?{" "}
+          <Link href="/auth/reset-password">Reset password</Link>
+        </p>
+      </FormFooter>
+    </section>
+  );
 }
