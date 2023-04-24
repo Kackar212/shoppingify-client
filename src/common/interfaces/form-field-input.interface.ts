1fc6f99e6b37af7927d1bd21fb57fe65c692d147
QoB <kackar212@gmail.com>
2023-04-25 00:22:15 +0200

    Refactor FormFieldInput interface

--- src/common/interfaces/form-field-input.interface.ts
+++ src/common/interfaces/form-field-input.interface.ts
@@ -1,13 +1,10 @@
-import {
-  InputHTMLAttributes,
-  SelectHTMLAttributes,
-  TextareaHTMLAttributes,
-} from "react";
+import { InputHTMLAttributes } from "react";
+import { HTMLInputTypeAttribute } from "../types";
 
 export interface FormFieldInput
   extends Omit<InputHTMLAttributes<HTMLInputElement>, "onChange"> {
   name: string;
   label: string;
-  type?: string;
+  type?: HTMLInputTypeAttribute | "textarea";
   placeholder?: string;
 }
