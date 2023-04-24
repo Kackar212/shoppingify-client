9f22a885ff62d51e52ae4da5fb9582e9f6aa924b
QoB <kackar212@gmail.com>
2023-04-24 23:46:16 +0200

    Create HTMLInputTypeAttribute type

--- src/common/types.ts
+++ src/common/types.ts
@@ -9,3 +9,27 @@ export type Query<T> = LazyQueryTrigger<
   QueryDefinition<any, any, any, ApiResponse<T>>
 >;
 export type QueryState<T> = TypedUseQueryStateResult<ApiResponse<T>, any, any>;
+export type HTMLInputTypeAttribute =
+  | "button"
+  | "checkbox"
+  | "color"
+  | "date"
+  | "datetime-local"
+  | "email"
+  | "file"
+  | "hidden"
+  | "image"
+  | "month"
+  | "number"
+  | "password"
+  | "radio"
+  | "range"
+  | "reset"
+  | "search"
+  | "submit"
+  | "tel"
+  | "text"
+  | "time"
+  | "url"
+  | "week"
+  | "textarea";
