b8a08a9f5f07ee29a615bb26da607274857c7cac
QoB <kackar212@gmail.com>
2023-04-29 06:10:07 +0200

    Refactor home page to redirect not logged in user to login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { api, getProducts } from "../src/features/api";
 import { wrapper } from "../src/features/store";
 import { Categories } from "../src/components/categories/categories.component";
 import { PrivatePage } from "../src/components/private-page/private-page.component";
+import { withAuth } from "../src/common/auth/with-auth";
 
 export default function Home() {
   return (
@@ -11,10 +12,14 @@ export default function Home() {
   );
 }
 
-Home.getInitialProps = wrapper.getInitialPageProps((store) => async () => {
-  store.dispatch(getProducts.initiate());
+export const getServerSideProps = wrapper.getServerSideProps(
+  withAuth(async ({ store }) => {
+    store.dispatch(getProducts.initiate());
 
-  await Promise.all(store.dispatch(api.util.getRunningQueriesThunk()));
+    await Promise.all(store.dispatch(api.util.getRunningQueriesThunk()));
 
-  return {};
-});
+    return {
+      props: {},
+    };
+  })
+);
