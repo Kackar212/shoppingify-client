941a9809b2bbb74988a92f32d1cde1c40dd9ca1b
QoB <kackar212@gmail.com>
2023-05-05 23:13:29 +0200

    Refactor Layout component to not refresh token

--- src/components/layout/layout.component.tsx
+++ src/components/layout/layout.component.tsx
@@ -1,10 +1,8 @@
-import { ReactNode, useEffect } from "react";
+import { ReactNode } from "react";
 import { Navigation } from "../navigation/navigation.component";
 import styles from "./layout.module.scss";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
-import { useRefreshTokenMutation } from "../../features/api";
-import { useRouter } from "next/router";
 import { ShoppingList } from "../shopping-list/shopping-list.component";
 import { HashRouter } from "../../common/context/hash-router/hash-router.provider";
 import { HashRoute } from "../hash-route/hash-route.component";
@@ -17,16 +15,6 @@ interface LayoutProps {
 }
 
 export function Layout({ children, className }: LayoutProps) {
-  const [refreshToken] = useRefreshTokenMutation();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!router.route.includes("/auth/")) {
-      refreshToken();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <div className={`${className} ${styles.container}`}>
       <Navigation />
