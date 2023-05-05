1974bc77f812c5cb7f0f3d1a2ac483bc1afb2b05
QoB <kackar212@gmail.com>
2023-05-05 22:24:28 +0200

    Add hash routes to Layout component

--- src/components/layout/layout.component.tsx
+++ src/components/layout/layout.component.tsx
@@ -6,6 +6,10 @@ import "react-toastify/dist/ReactToastify.min.css";
 import { useRefreshTokenMutation } from "../../features/api";
 import { useRouter } from "next/router";
 import { ShoppingList } from "../shopping-list/shopping-list.component";
+import { HashRouter } from "../../common/context/hash-router/hash-router.provider";
+import { HashRoute } from "../hash-route/hash-route.component";
+import { ProductDetails } from "../product-details/product-details.component";
+import { CreateProductForm } from "../create-product-form/create-product-form.component";
 
 interface LayoutProps {
   children: ReactNode;
@@ -28,6 +32,14 @@ export function Layout({ children, className }: LayoutProps) {
       <Navigation />
       <main className={styles.main}>{children}</main>
       <ShoppingList />
+      <HashRouter>
+        <HashRoute path="product/:name/:id">
+          <ProductDetails />
+        </HashRoute>
+        <HashRoute path="create-product">
+          <CreateProductForm />
+        </HashRoute>
+      </HashRouter>
       <ToastContainer
         position="top-right"
         autoClose={3000}
