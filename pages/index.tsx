12015252308a98c995b8c30e8d29984736822893
QoB <kackar212@gmail.com>
2023-05-18 00:27:42 +0200

    Add Header component to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,12 @@ import { wrapper } from "../src/features/store";
 import { Categories } from "../src/components/categories/categories.component";
 import { PrivatePage } from "../src/components/private-page/private-page.component";
 import { withAuth } from "../src/common/auth/with-auth";
+import { Header } from "../src/components/header/header.component";
 
 export default function Home() {
   return (
     <PrivatePage>
+      <Header />
       <Categories />
     </PrivatePage>
   );
