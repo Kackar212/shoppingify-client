bdf341d54319b2c75b419c674566542aa5557921
QoB <kackar212@gmail.com>
2023-05-28 23:19:36 +0200

    Add Space Mono font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import "../styles/normalize.css";
 import globalStyles from "../styles/global.module.scss";
 import { Layout } from "../src/components/layout/layout.component";
-import { Quicksand } from "@next/font/google";
+import { Quicksand, Space_Mono } from "@next/font/google";
 import { wrapper } from "../src/features/store";
 import { Provider } from "react-redux";
 
@@ -11,12 +11,20 @@ const quicksand = Quicksand({
   subsets: ["latin"],
 });
 
+const spaceMono = Space_Mono({
+  variable: "--font-space-mono",
+  weight: ["700"],
+  subsets: ["latin"],
+});
+
 export default function App({ Component, ...rest }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(rest);
 
   return (
     <Provider store={store}>
-      <Layout className={`${quicksand.className} ${globalStyles.app}`}>
+      <Layout
+        className={`${quicksand.className} ${spaceMono.variable} ${globalStyles.app}`}
+      >
         <Component {...props.pageProps} />
       </Layout>
     </Provider>
