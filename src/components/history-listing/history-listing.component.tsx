e3cb693c0c8bc1b88edda9269b36552781295f80
QoB <kackar212@gmail.com>
2023-06-04 23:24:46 +0200

    Create HistoryListing component

---
+++ src/components/history-listing/history-listing.component.tsx
@@ -0,0 +1,36 @@
+import { ShoppingList } from "../../common/interfaces/shopping-list.interface";
+import { UsePaginationResult } from "../../hooks/usePagination";
+import { HistoryShoppingList } from "../history-shopping-list/history-shopping-list.component";
+import { Paginator } from "../paginator/paginator.component";
+import styles from "./history-listing.module.scss";
+
+interface HistoryListingProps {
+  shoppingLists: Array<[string[], ShoppingList[]]>;
+  pagination: UsePaginationResult;
+}
+
+export function HistoryListing({
+  shoppingLists,
+  pagination,
+}: HistoryListingProps) {
+  return (
+    <article className={styles.article}>
+      <h2>History</h2>
+      {shoppingLists.map(([[month, year], lists]) => (
+        <section key={`${month},${year}`} className={styles.section}>
+          <header>
+            <h3 className={styles.heading}>
+              {month} {year}
+            </h3>
+          </header>
+          <ul className={styles.lists}>
+            {lists.map((list) => (
+              <HistoryShoppingList key={list.id} {...list} name={list.name!} />
+            ))}
+          </ul>
+        </section>
+      ))}
+      <Paginator {...pagination} />
+    </article>
+  );
+}
