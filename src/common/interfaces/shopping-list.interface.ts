2158cba8cd7c3bd2e106dabb10070400c3a63536
QoB <kackar212@gmail.com>
2023-06-11 01:47:39 +0200

    Refactor ShoppingList interface, now status has type of ListStatus

--- src/common/interfaces/shopping-list.interface.ts
+++ src/common/interfaces/shopping-list.interface.ts
@@ -1,9 +1,10 @@
 import { ShoppingListProduct } from "./shopping-list-product.interface";
+import { ListStatus } from "../types";
 
 export interface ShoppingList {
   id: number;
   name?: string;
-  status: string;
+  status: ListStatus;
   createdAt: string;
   updatedAt: string;
   products: ShoppingListProduct[];
