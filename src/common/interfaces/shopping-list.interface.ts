84fc2179e2d9d1fe1e908bbc0b75d3f471d3a280
QoB <kackar212@gmail.com>
2023-07-16 01:08:10 +0200

    Refactor ShoppingList interface

--- src/common/interfaces/shopping-list.interface.ts
+++ src/common/interfaces/shopping-list.interface.ts
@@ -1,5 +1,6 @@
 import { ShoppingListProduct } from "./shopping-list-product.interface";
 import { ListStatus } from "../types";
+import { ShoppingListUser } from "./shopping-list-user.interface";
 
 export interface ShoppingList {
   id: number;
@@ -8,4 +9,5 @@ export interface ShoppingList {
   createdAt: string;
   updatedAt: string;
   products: ShoppingListProduct[];
+  authorizedUsers: Array<ShoppingListUser>;
 }
