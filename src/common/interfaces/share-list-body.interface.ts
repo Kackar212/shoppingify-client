d893472558db69a9f3fa850136bfe55aba09759c
Kackar212 <kackar212@gmail.com>
2023-10-23 11:26:31 +0200

    Refactor ShareListBody interface

--- src/common/interfaces/share-list-body.interface.ts
+++ src/common/interfaces/share-list-body.interface.ts
@@ -2,5 +2,5 @@ import { Role } from "../enums";
 
 export interface ShareListBody {
   id: number;
-  users: Array<{ email: string; role: Role }>;
+  users: Array<{ email: string; role?: Role } | { name: string; role?: Role }>;
 }
