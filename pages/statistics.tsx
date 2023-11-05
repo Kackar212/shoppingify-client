f8923c69b2b8930248f5048434c2c47d19078d4d
Kackar212 <kackar212@gmail.com>
2023-11-05 18:11:34 +0100

    Create Statistics page

---
+++ pages/statistics.tsx
@@ -0,0 +1,38 @@
+import { withAuth } from "../src/common/auth/with-auth";
+import { getStatistics } from "../src/features/api";
+import { wrapper } from "../src/features/store";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  Legend,
+  LineElement,
+  LinearScale,
+  PointElement,
+  Title,
+  Tooltip,
+} from "chart.js";
+import { Statistics } from "../src/components/statistics/statistics.component";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export default function StatisticsPage() {
+  return <Statistics />;
+}
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  withAuth(async ({ store }) => {
+    await store.dispatch(getStatistics.initiate()).unwrap();
+
+    return {
+      props: {},
+    };
+  })
+);
