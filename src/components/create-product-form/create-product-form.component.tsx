6d4b1421cf522b2bccd078d366bea1edbd38869d
Kackar212 <kackar212@gmail.com>
2023-11-06 10:00:58 +0100

    Fix CreateProductForm component

--- src/components/create-product-form/create-product-form.component.tsx
+++ src/components/create-product-form/create-product-form.component.tsx
@@ -4,19 +4,18 @@ import { searchCategories, useCreateProductMutation } from "../../features/api";
 import { FormField } from "../form-field/form-field.component";
 import { Form } from "../form/form.component";
 import { Search } from "../search/search.component";
-import { useSearch } from "../../hooks/useSearch";
+import { Option, useSearch } from "../../hooks/useSearch";
 import { TextField } from "../text-field/text-field.component";
 import styles from "./create-product-form.module.scss";
 import { CreateProductBody } from "../../common/interfaces/create-product-body.interface";
-import { Category } from "../../common/interfaces/category.interface";
 import { Button } from "../button/button.component";
-import { Controller, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { API_SUCCESS, VALIDATION_MODE_ALL } from "../../common/constants";
-import { Error } from "../error/error.component";
 import { Sidebar } from "../sidebar/sidebar.component";
 import { useGetErrorMessage } from "../../hooks/useGetErrorMessage";
+import { SingleValue } from "react-select";
 
 const inputs: Array<FormFieldInput> = [
   {
@@ -32,38 +31,55 @@ const inputs: Array<FormFieldInput> = [
   { name: "image", label: "Image" },
 ];
 
+const categoryNameSchema = yup
+  .string()
+  .min(3, "Category must be at least 3 characters")
+  .max(80, "Category must be at most 80 characters")
+  .required("You must select category or create new!");
+
+const categorySchema = yup.object({
+  id: yup.number().integer().optional(),
+  name: yup
+    .string()
+    .min(3, "Category must be at least 3 characters")
+    .max(80, "Category must be at most 80 characters")
+    .required("You must select category or create new!"),
+});
+
 const createProductSchema = yup
   .object({
     name: yup.string().min(3).max(50).required(),
     note: yup.string().max(255),
     image: yup.string().url(),
-    category: yup
-      .object({
-        id: yup.number().integer().optional(),
-        name: yup
-          .string()
-          .min(3, "Category must be at least 3 characters")
-          .max(80, "Category must be at most 80 characters")
-          .required("You must select category or create new!"),
-      })
-      .required("You must select category or create new!"),
+    category: categorySchema,
   })
   .required();
+
+interface CreateProductFormValues extends Omit<CreateProductBody, "category"> {
+  category?: SingleValue<Option>;
+}
 
 export function CreateProductForm() {
   const [searchCategoriesByName, queryState] = searchCategories.useLazyQuery();
   const [createProduct, { isLoading, isSuccess, error }] =
     useCreateProductMutation();
-  const searchProps = useSearch({ query: searchCategoriesByName, queryState });
-  const formContextValue = useForm<CreateProductBody>({
+  const formContextValue = useForm<CreateProductFormValues>({
     resolver: yupResolver(createProductSchema),
     mode: VALIDATION_MODE_ALL,
   });
+
+  const searchProps = useSearch({
+    query: searchCategoriesByName,
+    queryState,
+  });
+
   const errorMessage = useGetErrorMessage(error);
 
   useEffect(() => {
     const isDirty = !!formContextValue.formState.dirtyFields.category;
-    const category = searchProps.value as Omit<Category, "products">;
+    const category = searchProps.value;
+
+    console.log("hmm", category);
 
     formContextValue.setValue("category", category, {
       shouldValidate: !!searchProps.value || isDirty,
@@ -73,10 +89,10 @@ export function CreateProductForm() {
   }, [searchProps.value]);
 
   const handleCreateProduct = useCallback(
-    async (createProductBody: CreateProductBody) => {
+    async (createProductBody: CreateProductFormValues) => {
       if (isLoading) return;
 
-      await createProduct(createProductBody).unwrap();
+      await createProduct(createProductBody as CreateProductBody).unwrap();
 
       formContextValue.reset();
       searchProps.reset();
@@ -87,7 +103,7 @@ export function CreateProductForm() {
 
   return (
     <Sidebar>
-      <Form
+      <Form<CreateProductFormValues>
         onSubmit={handleCreateProduct}
         className={styles.form}
         contextValue={formContextValue}
@@ -116,20 +132,37 @@ export function CreateProductForm() {
             )}
           />
         ))}
-        <Controller
-          name="category"
-          control={formContextValue.control}
-          render={({ field }) => (
-            <FormField
-              name="category"
-              label="Category"
-              required
-              render={({ name, errorId = "" }) => (
-                <>
-                  <Error name="category.name" errorId={errorId} />
-                  <Search {...searchProps} {...field} name={name} />
-                </>
-              )}
+        <FormField
+          name="category.name"
+          label="Category"
+          required
+          render={({ name, errorId = "" }) => (
+            <Search
+              {...searchProps}
+              name={name}
+              onInputChange={(value) => {
+                if (searchProps.value) return;
+
+                formContextValue.setValue(
+                  "category",
+                  {
+                    id: -1,
+                    name: value,
+                    value,
+                    label: value,
+                  },
+                  {
+                    shouldDirty: true,
+                    shouldValidate: true,
+                  }
+                );
+              }}
+              onChange={(value) => {
+                if (!value || !value.name) return;
+
+                formContextValue.setValue("category", value);
+                searchProps.onChange(value);
+              }}
             />
           )}
         />
